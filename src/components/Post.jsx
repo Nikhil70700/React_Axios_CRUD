791abf3ff94fc8e2a8be580530b0c568f9af0ba3
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-05-04 23:39:12 +0530

    Get all data Success

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getPost } from '../api/PostApi';
 
 const Post = () => {
-    const getPostData = async () => {
-        try {
-          const res = await getPost();
-          console.log("Posts:", res.data);
-          
-    
-        } catch (error) {
-          console.error("Error fetching posts:", error.message);
-        }
-      };
-    
-      useEffect(() => {
-        getPostData();  
-      }, []);
-    
-  return (
-    <div>Hello This is Post of CRUD From Axios</div>
-  )
+  const [data, setData] = useState([]);
+  const getPostData = async () => {
+    try {
+      const res = await getPost();
+      console.log("Posts:", res.data);
+      setData(res.data)
+
+    } catch (error) {
+      console.error("Error fetching posts:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  return <section className='section-post'>
+    <ol>
+      {
+        data.map((curElem) => {
+          const { id, body, title } = curElem;
+          return (
+            <li key={id}>
+              <p>Title:{title}</p>
+
+              <p>Body:{body}</p>
+              <button>Edit</button>
+              <button className='btn-delete'>Delete</button>
+
+            </li>
+          )
+        })
+      }
+    </ol>
+  </section>
 }
 
 export default Post
