e800cdffa9daea2f17751465cc18f8b95ddd6d17
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-05-06 01:02:27 +0530

    Http Get Data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,64 @@
-export const Form=()=>{
+import { useState } from "react"
+import { postData } from "../api/PostApi";
 
-    return(
-        <form>
+export const Form = ({ data, setData }) => {
+
+    const [addData, setAddData] = useState({
+        title: "",
+        body: "",
+    });
+    const handleInputChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setAddData((prev) => {
+            return {
+                ...prev, [name]: value,
+            }
+        })
+
+    }
+
+    const addPostData = async () => {
+        const res = await postData(addData)
+        if ((res.status === 201)) {
+            setData([...data, res.data])
+            setAddData({ title: "", body: "" })
+
+        }
+    }
+
+    // Form Submission
+    const handleFormData = (e) => {
+        e.preventDefault();
+        addPostData();
+    }
+    return (
+        <form onSubmit={handleFormData}>
             <div>
                 <label htmlFor="title"></label>
-                <input type="text" autoComplete="off" id="title" name="title" placeholder="Add title" />
+                <input type="text"
+                    autoComplete="off"
+                    id="title"
+                    name="title"
+                    placeholder="Add title"
+                    value={addData.title}
+                    onChange={handleInputChange}
+                />
             </div>
             <div>
                 <label htmlFor="body"></label>
-                <input type="text" autoComplete="off"
-                placeholder="Add post"
-                id="body"
-                name="body" />
+                <input type="text"
+                    autoComplete="off"
+                    id="body"
+                    name="body"
+                    placeholder="Add post"
+                    value={addData.value}
+                    onChange={handleInputChange}
+                />
             </div>
             <button type="submit">Add</button>
         </form>
     )
 }
+
